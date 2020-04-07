9d77903bb1568ab7c5de2628409597a8d3edaf01
Elisha Johnson <elishamagicjohnson@gmail.com>
2020-04-07 16:46:34 -0500

    removed pagination from search page & repopulated cuisine type list

--- src/main/webapp/app/search/search.tsx
+++ src/main/webapp/app/search/search.tsx
@@ -1,75 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import InfiniteScroll from 'react-infinite-scroller';
 import { connect } from 'react-redux';
 import { Link, RouteComponentProps } from 'react-router-dom';
 import { Button, Col, Row, Table } from 'reactstrap';
-import { Translate, ICrudGetAllAction, getSortState, IPaginationBaseState } from 'react-jhipster';
+import { Translate, ICrudGetAllAction } from 'react-jhipster';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { IRootState } from 'app/shared/reducers';
-import { getEntities, reset } from 'app/entities/restaurant/restaurant.reducer';
+import { getEntities } from 'app/entities/restaurant/restaurant.reducer';
 import { IRestaurant } from 'app/shared/model/restaurant.model';
 import { APP_DATE_FORMAT, APP_LOCAL_DATE_FORMAT } from 'app/config/constants';
-import { ITEMS_PER_PAGE } from 'app/shared/util/pagination.constants';
 
 export interface IRestaurantProps extends StateProps, DispatchProps, RouteComponentProps<{ url: string }> {}
 
 export const Search = (props: IRestaurantProps) => {
-  const [paginationState, setPaginationState] = useState(getSortState(props.location, ITEMS_PER_PAGE));
-  const [sorting, setSorting] = useState(false);
-
-  const getAllEntities = () => {
-    props.getEntities(paginationState.activePage - 1, paginationState.itemsPerPage, `${paginationState.sort},${paginationState.order}`);
-  };
-
-  const resetAll = () => {
-    props.reset();
-    setPaginationState({
-      ...paginationState,
-      activePage: 1
-    });
-  };
-
   useEffect(() => {
-    resetAll();
+    props.getEntities();
   }, []);
-
-  useEffect(() => {
-    if (props.updateSuccess) {
-      getAllEntities();
-    }
-  });
-
-  useEffect(() => {
-    getAllEntities();
-  }, [paginationState.activePage]);
-
-  const handleLoadMore = () => {
-    if (window.pageYOffset > 0) {
-      setPaginationState({
-        ...paginationState,
-        activePage: paginationState.activePage + 1
-      });
-    }
-  };
-
-  useEffect(() => {
-    if (sorting) {
-      getAllEntities();
-      setSorting(false);
-    }
-  }, [sorting]);
-
-  const sort = p => () => {
-    props.reset();
-    setPaginationState({
-      ...paginationState,
-      activePage: 1,
-      order: paginationState.order === 'asc' ? 'desc' : 'asc',
-      sort: p
-    });
-    setSorting(true);
-  };
 
   const { restaurantList, match, loading } = props;
   return (
@@ -83,86 +29,86 @@ export const Search = (props: IRestaurantProps) => {
         </Link>
       </h2>
       <div className="table-responsive">
-        <InfiniteScroll
-          pageStart={paginationState.activePage}
-          loadMore={handleLoadMore}
-          hasMore={paginationState.activePage - 1 < props.links.next}
-          loader={<div className="loader">Loading ...</div>}
-          threshold={0}
-          initialLoad={false}
-        >
-          {restaurantList && restaurantList.length > 0 ? (
-            <Table responsive>
-              <thead>
-                <tr>
-                  {/* <th className="hand" onClick={sort('id')}>
-                    <Translate contentKey="global.field.id">ID</Translate> <FontAwesomeIcon icon="sort" />
-                  </th> */}
-                  <th className="hand" onClick={sort('name')}>
-                    <Translate contentKey="yumzApp.restaurant.name">Name</Translate> <FontAwesomeIcon icon="sort" />
-                  </th>
-                  <th>
-                    <Translate contentKey="yumzApp.restaurant.cuisineType">Cuisine Type</Translate>
-                  </th>
-                  <th className="hand" onClick={sort('location')}>
-                    <Translate contentKey="yumzApp.restaurant.location">Location</Translate> <FontAwesomeIcon icon="sort" />
-                  </th>
-                  <th className="hand" onClick={sort('phone')}>
-                    <Translate contentKey="yumzApp.restaurant.phone">Phone</Translate> <FontAwesomeIcon icon="sort" />
-                  </th>
-                  <th className="hand" onClick={sort('website')}>
-                    <Translate contentKey="yumzApp.restaurant.website">Website</Translate> <FontAwesomeIcon icon="sort" />
-                  </th>
-                  <th />
-                </tr>
-              </thead>
-              <tbody>
-                {restaurantList.map((restaurant, i) => (
-                  <tr key={`entity-${i}`}>
-                    {/* <td>
-                      <Button tag={Link} to={`${match.url}/${restaurant.id}`} color="link" size="sm">
-                        {restaurant.id}
+        {restaurantList && restaurantList.length > 0 ? (
+          <Table responsive>
+            <thead>
+              <tr>
+                <th>
+                  <Translate contentKey="global.field.id">ID</Translate>
+                </th>
+                <th>
+                  <Translate contentKey="yumzApp.restaurant.name">Name</Translate>
+                </th>
+                <th>
+                  <Translate contentKey="yumzApp.restaurant.location">Location</Translate>
+                </th>
+                <th>
+                  <Translate contentKey="yumzApp.restaurant.phone">Phone</Translate>
+                </th>
+                <th>
+                  <Translate contentKey="yumzApp.restaurant.website">Website</Translate>
+                </th>
+                <th>
+                  <Translate contentKey="yumzApp.restaurant.cuisineType">Cuisine Type</Translate>
+                </th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {restaurantList.map((restaurant, i) => (
+                <tr key={`entity-${i}`}>
+                  <td>
+                    <Button tag={Link} to={`${match.url}/${restaurant.id}`} color="link" size="sm">
+                      {restaurant.id}
+                    </Button>
+                  </td>
+                  <td>{restaurant.name}</td>
+                  <td>{restaurant.location}</td>
+                  <td>{restaurant.phone}</td>
+                  <td>{restaurant.website}</td>
+                  <td>
+                    {restaurant.cuisineTypes
+                      ? restaurant.cuisineTypes.map((val, j) => (
+                          <span key={j}>
+                            <Link to={`cuisine-type/${val.id}`}>{val.name}</Link>
+                            {j === restaurant.cuisineTypes.length - 1 ? '' : ', '}
+                          </span>
+                        ))
+                      : null}
+                  </td>
+                  <td className="text-right">
+                    <div className="btn-group flex-btn-group-container">
+                      <Button tag={Link} to={`${match.url}/${restaurant.id}`} color="info" size="sm">
+                        <FontAwesomeIcon icon="eye" />{' '}
+                        <span className="d-none d-md-inline">
+                          <Translate contentKey="entity.action.view">View</Translate>
+                        </span>
                       </Button>
-                    </td> */}
-                    <td>{restaurant.name}</td>
-                    <td>{restaurant.cuisineTypes.map(cuisineType => cuisineType.name).join(", ")}</td>
-                    <td>{restaurant.location}</td>
-                    <td>{restaurant.phone}</td>
-                    <td>{restaurant.website}</td>
-                    <td className="text-right">
-                      <div className="btn-group flex-btn-group-container">
-                        <Button tag={Link} to={`${match.url}/${restaurant.id}`} color="info" size="sm">
-                          <FontAwesomeIcon icon="eye" />{' '}
-                          <span className="d-none d-md-inline">
-                            <Translate contentKey="entity.action.view">View</Translate>
-                          </span>
-                        </Button>
-                        <Button tag={Link} to={`${match.url}/${restaurant.id}/edit`} color="primary" size="sm">
-                          <FontAwesomeIcon icon="pencil-alt" />{' '}
-                          <span className="d-none d-md-inline">
-                            <Translate contentKey="entity.action.edit">Edit</Translate>
-                          </span>
-                        </Button>
-                        <Button tag={Link} to={`${match.url}/${restaurant.id}/delete`} color="danger" size="sm">
-                          <FontAwesomeIcon icon="trash" />{' '}
-                          <span className="d-none d-md-inline">
-                            <Translate contentKey="entity.action.delete">Delete</Translate>
-                          </span>
-                        </Button>
-                      </div>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
-          ) : (
-            !loading && (
-              <div className="alert alert-warning">
-                <Translate contentKey="yumzApp.restaurant.home.notFound">No Restaurants found</Translate>
-              </div>
-            )
-          )}
-        </InfiniteScroll>
+                      <Button tag={Link} to={`${match.url}/${restaurant.id}/edit`} color="primary" size="sm">
+                        <FontAwesomeIcon icon="pencil-alt" />{' '}
+                        <span className="d-none d-md-inline">
+                          <Translate contentKey="entity.action.edit">Edit</Translate>
+                        </span>
+                      </Button>
+                      <Button tag={Link} to={`${match.url}/${restaurant.id}/delete`} color="danger" size="sm">
+                        <FontAwesomeIcon icon="trash" />{' '}
+                        <span className="d-none d-md-inline">
+                          <Translate contentKey="entity.action.delete">Delete</Translate>
+                        </span>
+                      </Button>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        ) : (
+          !loading && (
+            <div className="alert alert-warning">
+              <Translate contentKey="yumzApp.restaurant.home.notFound">No Restaurants found</Translate>
+            </div>
+          )
+        )}
       </div>
     </div>
   );
@@ -170,16 +116,11 @@ export const Search = (props: IRestaurantProps) => {
 
 const mapStateToProps = ({ restaurant }: IRootState) => ({
   restaurantList: restaurant.entities,
-  loading: restaurant.loading,
-  totalItems: restaurant.totalItems,
-  links: restaurant.links,
-  entity: restaurant.entity,
-  updateSuccess: restaurant.updateSuccess
+  loading: restaurant.loading
 });
 
 const mapDispatchToProps = {
-  getEntities,
-  reset
+  getEntities
 };
 
 type StateProps = ReturnType<typeof mapStateToProps>;
